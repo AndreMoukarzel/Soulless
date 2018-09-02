07fd340cc4bf937245e5d97a809ecdf58a52ace4
Andre Ferrari Moukarzel <andremoukarzel@hotmail.com>
2018-09-02 17:34:41 -0300

    Created basic test scene

---
+++ Test.gd
@@ -0,0 +1,34 @@
+extends Node
+
+var Chars = ["Bunny", "Mole"]
+
+func _ready():
+	get_node("Display").set_position(OS.window_size/2 + Vector2(150, 0))
+	
+	# Test each char
+	for c in Chars:
+		var unit = instance_unit(c)
+		display_unit_info(unit)
+		print_unit_info(unit)
+#		unit.queue_free()
+
+
+func instance_unit(unit_name):
+	var path = "res://Characters/" + unit_name + "/" + unit_name + ".tscn"
+	var l = load(path)
+	var unit = l.instance()
+	unit.set_position(OS.window_size/2)
+	add_child(unit)
+	
+	return unit
+
+func display_unit_info(unit):
+	get_node("Display/VBoxContainer/Name").set_text(unit.name)
+	get_node("Display/VBoxContainer/HP").set_text(str(unit.HP))
+	get_node("Display/VBoxContainer/ATK").set_text(str(unit.ATK))
+	get_node("Display/VBoxContainer/DEF").set_text(str(unit.DEF))
+	
+func print_unit_info(unit):
+	var unit_info = unit.name + ":\nHP: %s\nATK: %s\nDEF: %s\n"
+	unit_info = unit_info % [str(unit.HP), str(unit.ATK), str(unit.DEF)]
+	print(unit_info)
