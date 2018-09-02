5a9d1ef6c7d0a76a69f1adfbc96ee6beef3b9991
Andre Ferrari Moukarzel <andremoukarzel@hotmail.com>
2018-09-02 16:46:05 -0300

    Started Unit resctructure
    Unit structure was not taking advantage of Godot's node system. Now,
    each unit has basic info at it's own scene. Now saving animations
    individually and having a unit database is no longer needed.
    
    Also, they don't have arms anymore!

---
+++ Characters/BaseChar.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+export(float, 0.1, 10.0, 0.1) var Size = 1.0
+export(int) var HP = 1
+export(int) var ATK = 1
+export(int) var DEF = 1
+export(String, "Bunny", "Mole", "Ent") var Race
+export(String) var Signature1
+export(String) var Signature2
