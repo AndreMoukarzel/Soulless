ca29b7f5ac1f0255a51746a0bedee68281378a02
Andre Ferrari Moukarzel <andremoukarzel@hotmail.com>
2018-09-02 18:29:25 -0300

    Test scene now plays animations

--- Test.gd
+++ Test.gd
@@ -1,16 +1,20 @@
 extends Node
 
+signal animations_finished
+
 var Chars = ["Bunny", "Mole"]
 
 func _ready():
-	get_node("Display").set_position(OS.window_size/2 + Vector2(150, 0))
+	get_node("Display").set_position(OS.window_size/2 + Vector2(0, 200))
 	
 	# Test each char
 	for c in Chars:
 		var unit = instance_unit(c)
 		display_unit_info(unit)
 		print_unit_info(unit)
-#		unit.queue_free()
+		display_animations(unit)
+		yield(self, "animations_finished")
+		unit.queue_free()
 
 
 func instance_unit(unit_name):
@@ -27,8 +31,18 @@ func display_unit_info(unit):
 	get_node("Display/VBoxContainer/HP").set_text(str(unit.HP))
 	get_node("Display/VBoxContainer/ATK").set_text(str(unit.ATK))
 	get_node("Display/VBoxContainer/DEF").set_text(str(unit.DEF))
-	
+
 func print_unit_info(unit):
 	var unit_info = unit.name + ":\nHP: %s\nATK: %s\nDEF: %s\n"
 	unit_info = unit_info % [str(unit.HP), str(unit.ATK), str(unit.DEF)]
 	print(unit_info)
+
+func display_animations(unit):
+	var anim_player = unit.get_node("AnimationPlayer")
+	var anim_list = anim_player.get_animation_list()
+	
+	for anim in anim_list:
+		$AnimationTimer.start()
+		anim_player.play(anim)
+		yield($AnimationTimer, "timeout")
+	emit_signal("animations_finished")
