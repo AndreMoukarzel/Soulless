414452e24b15db898b4d0d4d685c2e746492952b
Andre Ferrari Moukarzel <andremoukarzel@hotmail.com>
2018-10-16 17:03:19 -0300

    Closes #16
    AI attacks weakest unit avaiable

--- Combat/EnemyAI.gd
+++ Combat/EnemyAI.gd
@@ -1,6 +1,19 @@
 
 static func choose_action(activeUnit, Allies, Enemies): # the AI controls the Enemies, obviously
 	var targetable = Allies.get_alive_units()
+	var i = find_weakest_unit(targetable)
 	
-	var i = randi() % targetable.size()
 	return [activeUnit.Signature1, targetable[i]]
+
+
+func find_weakest_unit(Units):
+	var weakest = Units[0]
+	var weakest_val = weakest.HP + weakest.DEF * 0.5
+	
+	for U in Units:
+		var u_val = U.HP + U.DEF * 0.5
+		if weakest_val > u_val:
+			weakest = U
+			weakest_val = u_val
+	
+	return Units.find(weakest)
